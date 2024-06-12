3df9a8ed9b3dc918387090d6d8ee742eb34f4fd4
David Hormigo <dhorram948@g.educaand.es>
2024-06-12 19:01:56 +0200

    navegacion back y boton share

--- app/src/main/java/com/alanturing/cpifp/rickandmorty/character/CharacterDetailFragment.kt
+++ app/src/main/java/com/alanturing/cpifp/rickandmorty/character/CharacterDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.alanturing.cpifp.rickandmorty.character
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +8,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import coil.load
+import com.alanturing.cpifp.rickandmorty.R
 import com.alanturing.cpifp.rickandmorty.databinding.FragmentCharacterDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -16,6 +19,7 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class CharacterDetailFragment():Fragment() {
     private lateinit var binding: FragmentCharacterDetailBinding
+    private lateinit var characterDetailUi: CharacterDetailUi
     private val arguments: CharacterDetailFragmentArgs by navArgs()
     private val viewModel: CharacterViewModel by viewModels()
     override fun onCreateView(
@@ -36,6 +40,7 @@ class CharacterDetailFragment():Fragment() {
         val id = arguments.id
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.getCharacter(id)?.let {
+                characterDetailUi = it
                 binding.detailToolbar.title = it.name
                 binding.characterImage.load(it.image)
                 binding.characterSpecies.text = it.species
@@ -43,6 +48,26 @@ class CharacterDetailFragment():Fragment() {
                 binding.characterGender.text = it.gender
             }
 
+        }
+        binding.detailToolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding.detailToolbar.setOnMenuItemClickListener {
+            item ->
+            when (item.itemId) {
+                R.id.share -> {
+                    val sendIntent: Intent = Intent().apply {
+                        action = Intent.ACTION_SEND
+                        putExtra(Intent.EXTRA_TEXT, "${characterDetailUi.name} (${characterDetailUi.gender} - ${characterDetailUi.species} - ${characterDetailUi.status})")
+                        type = "text/plain"
+                    }
+
+                    val shareIntent = Intent.createChooser(sendIntent, null)
+                    startActivity(shareIntent)
+                    true
+                }
+                else -> false
+            }
         }
 
 
